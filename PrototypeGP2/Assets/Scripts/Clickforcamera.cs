3901c51206e40d729a7c992243fd442d3b7892f7
Larzbarzcarz <bjdalexander.larsson@gmail.com>
2026-01-29 11:01:59 +0100

    Made the final polishing steps to the mainbase, and ALPHA scene is the one we are presenting with

--- PrototypeGP2/Assets/Scripts/Clickforcamera.cs
+++ PrototypeGP2/Assets/Scripts/Clickforcamera.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Clickforcamera : MonoBehaviour
 {
     public GameObject mainCamera;
     public GameObject craftingCamera;
-
+    public GameObject LabUi;
+    public GameObject Return;
+    
     void Start()
     {
+        Return.SetActive(false);
         mainCamera.SetActive(true);
         craftingCamera.SetActive(false);
     }
 
     public void SwitchToCrafting()
     {
+        Return.SetActive(true);
+        LabUi.SetActive(false);
         Debug.Log("Switching to crafting camera");
         mainCamera.SetActive(false);
         craftingCamera.SetActive(true);
@@ -20,8 +26,17 @@ public class Clickforcamera : MonoBehaviour
 
     public void SwitchToMain()
     {
+        Return.SetActive(false);
+        LabUi.SetActive(true);
         Debug.Log("Switching to main camera");
         craftingCamera.SetActive(false);
         mainCamera.SetActive(true);
     }
+
+    public void SwitchToCombat()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    
 }
