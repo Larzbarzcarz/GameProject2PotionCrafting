64775446ddb400fdaa242b03c1b584c0b696a2f0
Larzbarzcarz <bjdalexander.larsson@gmail.com>
2026-02-05 11:51:16 +0100

    Potioncrafting tweaks

---
+++ PrototypeGP2/Assets/ItemDragController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+
+
+public class ItemDragController : MonoBehaviour
+{
+    private DragItem dragItem;
+    public Camera cam;
+
+    void Awake()
+    {
+     	dragItem = GetComponentInParent<DragItem>();
+        cam = dragItem.cam != null ? dragItem.cam : Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            TryBeginDrag();
+
+        if (Input.GetMouseButton(0))
+            dragItem.Drag(Input.mousePosition);
+
+        if (Input.GetMouseButtonUp(0))
+            dragItem.EndDrag();
+    }
+
+    void TryBeginDrag()
+    {
+		Camera cam = dragItem.cam; 
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.transform == transform)
+                dragItem.BeginDrag(hit.point);
+        }
+    }
+}
+
+
