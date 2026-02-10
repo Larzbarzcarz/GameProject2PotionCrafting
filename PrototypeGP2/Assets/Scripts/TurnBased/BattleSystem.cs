7de9ba01d825211d3f458922fd2548121020d746
Squeegee <q@neozoo.com>
2026-02-10 17:03:16 +0100

    My final push

--- PrototypeGP2/Assets/Scripts/TurnBased/BattleSystem.cs
+++ PrototypeGP2/Assets/Scripts/TurnBased/BattleSystem.cs
@@ -344,6 +344,7 @@ public class BattleSystem : MonoBehaviour
         {
             dialogueText.text = "You Lose!";
             OnBattleLost?.Invoke();
+            AudioManager.Instance.PlayOneShot(FMODEvents.instance.loss);
         }
     }
  
