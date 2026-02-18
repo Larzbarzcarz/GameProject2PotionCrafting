797117770ae5b8e9989337622cbe0142f5fe324b
Vanillifer <166023452+Vanillifer@users.noreply.github.com>
2026-02-18 18:06:36 +0100

    crafting rename fix

--- PrototypeGP2/Assets/Scripts/Brewing Mechanic/Recipe's/RecipeSO/PotionRenameUI.cs
+++ PrototypeGP2/Assets/Scripts/Brewing Mechanic/Recipe's/RecipeSO/PotionRenameUI.cs
@@ -31,8 +31,12 @@ public class PotionRenameUI : MonoBehaviour
 
     public void Yes()
     {
+        Debug.Log("[RENAME] YES clicked");
+
         if (promptPanel != null) promptPanel.SetActive(false);
         if (renamePanel != null) renamePanel.SetActive(true);
+
+        Debug.Log($"[RENAME] renamePanel active = {renamePanel != null && renamePanel.activeSelf}");
 
         if (nameRegistry != null && renameInput != null)
         {
